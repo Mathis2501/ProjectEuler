2a3f1301ec69eecad0b5b809440924c1c5cc3af8
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2017-02-09 11:27:24 +0100

    fuck

---
+++ Large_Sum/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Large_Sum;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LargeSum ls = new LargeSum();
+
+            ls.sum();
+
+            Console.Read();
+        }
+    }
+}
---
+++ SummationOfPrimes/SummationOfPrimes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummationOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        public void Run()
+        {
+            bool[] name = new bool[100-1];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                name[i] = true;
+                
+            }
+            
+            bool running = true;
+            int p = 2;
+            while (running)
+            {
+                for (int i = p; i < name.Length; i+=p)
+                {
+                    name[i] = false;
+                    
+                }
+                int tempP = p;
+                for (int i = p; i < name.Length; i++)
+                {
+                    if (name[i] == true && p < i+2)
+                    {
+                        p = i+2;
+                    }
+                }
+                if (tempP == p)
+                {
+                    running = false;
+                }
+                
+            }
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i] == true)
+                {
+                    Console.WriteLine(i+2);
+                }
+
+            }
+            Console.Read();
+            
+        }
+    }
+}
